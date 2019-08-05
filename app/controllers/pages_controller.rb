f8fe4801f853346fa1955a8ef8f1afe6c6937cf3
Marjone <marjonedev@gmail.com>
2019-08-05 17:17:53 +0000

    Creating pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def features
+  end
+
+  def pricing
+  end
+
+  def signup
+  end
+
+  def documentation
+  end
+
+  def faq
+  end
+
+  def security
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-	get 'home/index'
+  get 'pages/home'
+  get 'features', to: 'pages#features'
+  get 'pricing', to: 'pages#pricing'
+  get 'signup', to: 'pages#signup'
+  get 'documentation', to: 'pages#documentation'
+  get 'faq', to: 'pages#faq'
+  get 'security', to: 'pages#security'
+  get 'signup', to: 'pages#signup'
  
-	root 'home#index'
+  root 'pages#home'
 end
+
