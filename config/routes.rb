afbcb5b7db8d4bbfa07c85def190b92f343dbc1e
Marjone <marjonedev@gmail.com>
2019-08-10 01:04:45 +0800

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'features', to: 'pages#features'
   get 'pricing', to: 'pages#pricing'
-  get 'signup', to: 'pages#signup'
   get 'documentation', to: 'pages#documentation'
   get 'faqs', to: 'pages#faqs'
   get 'security', to: 'pages#security'
-  # get 'signup', to: 'users#new'
-
-  get 'users/new'
+  get 'signup', to: 'users#new'
  
   root 'pages#home'
 end
