56444793f8bc6ae95b92557ca8084f8c2755f006
ryankopf <ryan@ryankopf.com>
2019-08-10 00:13:38 -0500

    Updating users configuration, step 1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+  # has_many :emailaccounts
+  # has_many :autoreplies, through: :emailaccounts
+
+  attr_accessor :password
+
+  validates_presence_of :username
+  validates_presence_of :password, :if => :password_required?
+  validates_length_of :password, :within => 8..40, :if => :password_required?
+  validates_length_of :username, :within => 5..40
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates_format_of :email_address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :username, :with => /\A[a-z][a-z0-9\_]*?\Z/, :message => "must start with a letter and include only letters, numbers, and underscore."
+
+  before_save :encrypt_password
+  before_validation :lowercase_username
+
+  def lowercase_username
+    self.username.downcase!
+  end
+
+  def encrypt(password)
+    self.class.encrypt(password, salt)
+  end
+
+  def self.encrypt(password,salt)
+    Digest::SHA512.hexdigest("--#{salt}---#{password}--")
+  end
+
+  protected
+    # before filter
+    def encrypt_password
+      return if password.blank?
+      self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}---#{username}--") if new_record?
+      self.crypted_password = encrypt(password)
+    end
+
+    def password_required?
+      crypted_password.blank? || !password.blank?
+    end
 end
--- db/migrate/20190808170210_create_users.rb
+++ db/migrate/20190808170210_create_users.rb
@@ -3,11 +3,12 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :password
+      t.string :encrypted_password
       t.string :salt
 
       t.timestamps
     end
+    add_index :users, :username
     add_index :users, :email
   end
 end
