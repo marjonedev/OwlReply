0a00d52f138bd0205209e679998e0d7f817bce12
Marjone <marjonedev@gmail.com>
2019-08-14 00:39:22 +0800

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: {
-          status: :created,
-          logged_in: true,
-          user: user
-      }
-      # redirect_to root_url, notice: "Logged in!"
+      redirect_to root_url, notice: "Logged in!"
     else
       flash.now[:alert] = "Username or password is invalid"
-      render json: {
-          status: 401
-      }
-      # render "new"
+      render "new"
     end
   end
 
