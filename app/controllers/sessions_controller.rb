bb24458651ecb3ffaebda634bcd47fbf37d426ef
Marjone <marjonedev@gmail.com>
2019-08-14 01:09:09 +0800

    fix user parameters upon login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
