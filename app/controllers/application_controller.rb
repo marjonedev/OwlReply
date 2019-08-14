ed62bb2a0c889df6d0c88e8435b673c0ef56f7e9
Marjone <marjonedev@gmail.com>
2019-08-15 01:10:26 +0800

    render 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def render_not_found
     # render :file => "#{Rails.root}/public/404.html",  :status => 404, :layout => "main"
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found }
+      format.html { render template: "#{Rails.root}/public/404.html", layout: "main", status: not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
