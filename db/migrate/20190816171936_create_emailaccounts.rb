73caa0d7cd42615971ca057b2ffb301596af74ae
Marjone <marjonedev@gmail.com>
2019-08-17 01:37:05 +0800

    emailaccount migration to create foreign_key

--- db/migrate/20190816171936_create_emailaccounts.rb
+++ db/migrate/20190816171936_create_emailaccounts.rb
@@ -1,7 +1,7 @@
 class CreateEmailaccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :emailaccounts do |t|
-      t.integer :user_id
+      t.references :users, foreign_key: true
       t.string :address
       t.string :password
       t.string :encrypted_password
