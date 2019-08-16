2bf5a7f3d118809e00f996659c806796d857d6f5
Marjone <marjonedev@gmail.com>
2019-08-17 01:57:00 +0800

    emailaccount change user_id column

--- db/migrate/20190816174731_change_user_on_emailaccount.rb
+++ db/migrate/20190816174731_change_user_on_emailaccount.rb
@@ -1,6 +1,5 @@
 class ChangeUserOnEmailaccount < ActiveRecord::Migration[5.2]
   def change
-    rename_column :emailaccounts, :users_id, :user_id
     change_column :emailaccounts, :user_id, :integer, null: false
   end
 end
