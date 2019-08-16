608fdefd173e803754043137c746416454fd8912
Marjone <marjonedev@gmail.com>
2019-08-16 22:31:48 +0800

    error message on invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Username or password is invalid"
-      # flash.now[:user] = "username: " + params[:username] + ", password: " + params[:password]
-      # flash.now[:user_find] = user
+      flash.now[:errormsg] = "Username or password is invalid"
       render "new"
     end
   end
