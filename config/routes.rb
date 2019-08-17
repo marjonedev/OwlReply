dc4dd8d70d24061f6d4a840ea253228608ed948b
ryankopf <ryan@ryankopf.com>
2019-08-17 00:32:48 -0500

    Updating email design.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :emailaccounts
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unacceptable"
   get "/500", to: "errors#internal_error"
 
-  resources :users
+  resources :emailaccounts
   resources :sessions
+  resources :users
 
   root 'pages#home'
 
