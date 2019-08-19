51d20db5ccb8cc10cdb7273ccfb4fa63ec00199d
Marjone <marjonedev@gmail.com>
2019-08-20 01:49:50 +0800

    adding validation

--- app/models/emailaccount.rb
+++ app/models/emailaccount.rb
@@ -1,3 +1,8 @@
 class Emailaccount < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :password, :if => :password_required?
+  validates_presence_of :address
+  validates_format_of :address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
 end
