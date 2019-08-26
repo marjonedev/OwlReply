4410744c5142cfd65a26ca3f6292980e58bbbc6c
Marjone <marjonedev@gmail.com>
2019-08-27 03:29:59 +0800

    DB add columns to user

--- db/migrate/20190826192648_add_user_columns.rb
+++ db/migrate/20190826192648_add_user_columns.rb
@@ -1,4 +1,7 @@
 class AddUserColumns < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :subscription_level, :integer
+    add_column :users, :subscription_start_date, :datetime
+    add_column :users, :subscription_last_payment_date, :datetime
   end
 end
