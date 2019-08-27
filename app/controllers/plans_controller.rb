a470322497807644496b089e6e8003bf172c5f28
ryankopf <ryan@ryankopf.com>
2019-08-26 22:14:56 -0500

    Updating.

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,20 @@
+class PlansController < ApplicationController
+  before_action :set_subscription, only: [:show, :edit, :update, :destroy]
+  # This is a Controller-Only function that sort of wraps the "Subscription".
+  # This user-facing interface is what allows a user to change their subscription plan.
+
+  def index
+    @plans = Subscription.where(secret: false)
+  end
+
+  def update
+    current_user.set_subscription!(@subscription)
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_subscription
+    @subscription = Subscription.find(params[:id])
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,20 @@ class User < ApplicationRecord
     Emailaccount.create(user_id: self.id, address: self.email_address)
   end
 
+  def current_subscription_id
+    return nil if self.invoices.empty?
+    self.invoices.first.subscription_id
+  end
+
+  def set_subscription!(subscription)
+    self.update_attribute(:subscription_id,subscription.id)
+    self.generate_first_invoice! if current_subscription_id.nil?
+  end
+
+  def generate_first_invoice!
+    self.invoices.create(subscription_id: self.subscription_id) if current_subscription_id.nil? # Amount, date, and collecting payment too, will be done by the invoice object 'on_create'.
+  end
+
   def lowercase_username
     self.username.downcase!
   end
