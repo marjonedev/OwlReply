1db5c7a156dffe81dfa4d84e797d9a9d3d6dda95
Marjone <marjonedev@gmail.com>
2019-08-28 22:28:27 +0800

    notification plugin

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,11 +5,15 @@ $(function(){
 
     $.Custom = {
         notify: function(message, type = 'successmsg') {
-            $('.floating-notification')
+            var floating = $('.floating-notification')
                 .append('<div class="style-msg '+type+'">\n' +
                     '<div class="sb-msg">'+message+'</div>' +
                     '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\n' +
                     '</div>');
+
+            $(floating).on('click', '.close', function () {
+                $(this).closest('.style-msg').remove();
+            });
         }
     }
 });
