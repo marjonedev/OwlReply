a12665c64aba89de067d15f4bf7f4393eef0f024
Marjone <marjonedev@gmail.com>
2019-08-29 00:25:24 +0800

    db migrate add columns

--- db/migrate/20190828154049_add_columns_emailaccount.rb
+++ db/migrate/20190828154049_add_columns_emailaccount.rb
@@ -1,4 +1,9 @@
 class AddColumnsEmailaccount < ActiveRecord::Migration[5.2]
   def change
+    add_column :emailaccounts, :drafts_created_today, :integer
+    add_column :emailaccounts, :drafts_created_lifetime, :integer
+    add_column :emailaccounts, :template, :text
+    add_column :replies, :drafts_created_today, :integer
+    add_column :replies, :drafts_created_lifetime, :integer
   end
 end
