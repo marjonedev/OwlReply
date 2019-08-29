abe3d568acc47a217c6a66369ce096a6a6c9709a
Marjone <marjonedev@gmail.com>
2019-08-29 17:07:55 +0000

    Paymentmethods controller

---
+++ app/controllers/paymentmethods_controller.rb
@@ -0,0 +1,2 @@
+class PaymentmethodsController < ApplicationController
+end
--- test/controllers/paymentmethods_controller_test.rb
+++ test/controllers/paymentmethods_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PaymentmethodControllerTest < ActionDispatch::IntegrationTest
+class PaymentmethodsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
