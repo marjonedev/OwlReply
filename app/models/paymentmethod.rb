4d622ce48dca90270e49c14217bc5ffff3c4611d
Marjone <marjonedev@gmail.com>
2019-08-30 01:36:48 +0800

    payment method model

--- app/models/paymentmethod.rb
+++ app/models/paymentmethod.rb
@@ -1,2 +1,3 @@
 class Paymentmethod < ApplicationRecord
+  belongs_to :user
 end
--- db/migrate/20190828163641_add_column_paymentmethod.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnPaymentmethod < ActiveRecord::Migration[5.2]
-  def change
-    add_column :paymentmethods, :customer_id, :string
-  end
-end
