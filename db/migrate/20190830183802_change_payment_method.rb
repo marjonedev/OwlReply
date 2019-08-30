056d556bb8de559055abfa47a6f0e5ccb45401e6
Marjone <marjonedev@gmail.com>
2019-08-31 02:42:50 +0800

    db migration

--- db/migrate/20190830183802_change_payment_method.rb
+++ db/migrate/20190830183802_change_payment_method.rb
@@ -1,4 +1,21 @@
 class ChangePaymentMethod < ActiveRecord::Migration[5.2]
   def change
+    remove_column :paymentmethods, :card_type
+    remove_column :paymentmethods, :card_name
+    add_column :paymentmethods, :card_brand, :string
+    add_column :paymentmethods, :card_funding, :string
   end
 end
+
+# t.integer "user_id"
+# t.string "method"
+# t.string "card_name"
+# t.string "card_type"
+# t.string "customer_id"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+# t.boolean "default", default: false
+# t.string "card_number"
+# t.string "card_cvc"
+# t.string "card_exp_month"
+# t.string "card_exp_year"
