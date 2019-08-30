30ebc475381ef53c77d71542ab82b31b02476bc4
Marjone <marjonedev@gmail.com>
2019-08-31 01:49:18 +0800

    payment method migration

--- db/migrate/20190830173412_add_column_payment_methods.rb
+++ db/migrate/20190830173412_add_column_payment_methods.rb
@@ -1,8 +1,5 @@
 class AddColumnPaymentMethods < ActiveRecord::Migration[5.2]
   def change
-    add_column :paymentmethods, :default, :boolean, :default => false
-    add_column :paymentmethods, :card_number, :string
-    add_column :paymentmethods, :card_cvc, :string
     add_column :paymentmethods, card_exp_month, :string
     add_column :paymentmethods, card_exp_year, :string
     remove_column :paymentmethods, :card_exp_date
