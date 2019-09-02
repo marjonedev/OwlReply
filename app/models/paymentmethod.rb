d6f1ca7ba37165f3e3d0db660aebcb8e325925c8
Marjone <marjonedev@gmail.com>
2019-09-03 01:33:26 +0800

    update default

--- app/models/paymentmethod.rb
+++ app/models/paymentmethod.rb
@@ -1,3 +1,11 @@
 class Paymentmethod < ApplicationRecord
   belongs_to :user
+
+  after_save :set_default
+
+  def set_default
+    if self.default
+      Paymentmethod.where(user_id: self.user_id).where.not(id: id).update_all(default: false)
+    end
+  end
 end
