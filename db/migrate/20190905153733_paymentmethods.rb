ab08f4de050ba45194d2404d282223976350049c
Marjone <marjonedev@gmail.com>
2019-09-06 00:02:44 +0800

    change paymentmethod columns

--- db/migrate/20190905153733_paymentmethods.rb
+++ db/migrate/20190905153733_paymentmethods.rb
@@ -1,4 +1,9 @@
 class Paymentmethods < ActiveRecord::Migration[5.2]
   def change
+    rename_column :paymentmethods, :customer_id, :token
+    remove_column :paymentmethods, :card_brand
+    remove_column :paymentmethods, :method
+    remove_column :paymentmethods, :card_cvc
+    remove_column :paymentmethods, :card_funding
   end
 end
