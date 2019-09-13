1055a0a216480a0b6dbfe70da6cd2496ef218e10
Marjone <marjonedev@gmail.com>
2019-09-13 23:11:02 +0800

    invoice number

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,13 +4,18 @@ class Invoice < ApplicationRecord
   has_many :transactions
   before_create :set_details
   after_create :charge_card #todo: charge card after create
-  after_create :set_next_subscription_date
+  after_create :set_next_subscription_date, :set_invoice_number
   attr_accessor :previous_price
 
   def set_details
     self.amount = self.subscription.price
     self.date = Date.today
     self.amount = self.amount - self.previous_price unless (self.previous_price.nil? || (self.previous_price > self.amount))
+  end
+
+  def set_invoice_number
+    number = "INV/#{Date.current.year}/%.4i" % self.id
+    self.update_attribute(:number, number)
   end
 
   def charge_card
