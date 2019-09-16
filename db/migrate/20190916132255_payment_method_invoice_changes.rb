5aba6d4606899497332341580e31bb3a6e3e1f49
Marjone <marjonedev@gmail.com>
2019-09-16 21:42:10 +0800

    db migration

--- db/migrate/20190916132255_payment_method_invoice_changes.rb
+++ db/migrate/20190916132255_payment_method_invoice_changes.rb
@@ -1,4 +1,9 @@
 class PaymentMethodInvoiceChanges < ActiveRecord::Migration[5.2]
   def change
+    change_column :invoices, :amount_paid, :integer
+    change_column :invoices, :amount, :integer
+    change_column :transactions, :amount, :integer
+    add_column :invoices, :payment_currency, :string
+    add_column :paymentmethods, :currency, :string
   end
 end
