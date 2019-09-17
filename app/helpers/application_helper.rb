173bea786ca634c80717e441fd1d658accab5e2c
Marjone <marjonedev@gmail.com>
2019-09-18 01:02:06 +0800

    admin dashboard functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
     s.html_safe
   end
 
+  def number_of_accounts
+    User.where(:admin => false).count
+  end
+
+  def number_of_email_accounts
+    Emailaccount.joins("left join users on user.id = emailaccounts.user_id").where("users.admin is 0").count
+  end
+
+  def number_of_replies
+    Reply.count
+  end
+
 end
