a48eb69e2d28d82c77734e237d58676a51f0dbb8
ryankopf <ryan@ryankopf.com>
2019-09-17 00:11:50 -0500

    Adding google analytics.

--- db/migrate/2019091701_add_column_error_emailaccount.rb
+++ db/migrate/2019091701_add_column_error_emailaccount.rb
@@ -1,5 +1,5 @@
 class AddColumnErrorEmailaccount < ActiveRecord::Migration[5.2]
   def change
-    add_column :emailaccount, :error, :string
+    add_column :emailaccounts, :error, :string
   end
 end
