839f897ce0001195846d1250ff536aea7f7ca3ec
Marjone <marjonedev@gmail.com>
2019-09-19 00:44:27 +0800

    admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,18 @@
 class AdminController < ApplicationController
   before_action :verify_admin
 
+  def accounts
+    @accounts = User.where(:admin => false)
+  end
+
+  def emailaccounts
+    @emailaccounts =  Emailaccount.left_joins(:user).where(users: {admin: false})
+  end
+
+  def replies
+    @replies = Reply.all
+  end
+
   private
   def verify_admin
     unless current_user.admin
