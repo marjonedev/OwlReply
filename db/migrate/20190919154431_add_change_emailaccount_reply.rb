726304624f063ab33af19e26af7263ad3e078e95
Marjone <marjonedev@gmail.com>
2019-09-19 23:48:23 +0800

    db migration

--- db/migrate/20190919154431_add_change_emailaccount_reply.rb
+++ db/migrate/20190919154431_add_change_emailaccount_reply.rb
@@ -1,4 +1,6 @@
 class AddChangeEmailaccountReply < ActiveRecord::Migration[5.2]
   def change
+    add_column :emailaccounts, :drafts_missing_replies, :integer
+    add_column :replies, :search, :string
   end
 end
