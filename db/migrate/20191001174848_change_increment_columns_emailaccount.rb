9d4e01d8252cb02570a0c525f219182fe950997c
Marjone <marjonedev@gmail.com>
2019-10-02 01:52:12 +0800

    db migrate

--- db/migrate/20191001174848_change_increment_columns_emailaccount.rb
+++ db/migrate/20191001174848_change_increment_columns_emailaccount.rb
@@ -1,4 +1,6 @@
 class ChangeIncrementColumnsEmailaccount < ActiveRecord::Migration[5.2]
   def change
+    rename_column :emailaccounts, :drafts_missing_replies, :drafts_missing_replies_lifetime
+    add_column :emailaccounts, :drafts_missing_replies_today, :integer
   end
 end
