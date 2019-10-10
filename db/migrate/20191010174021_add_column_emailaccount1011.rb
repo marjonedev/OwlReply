fdcb4173aee6c4a4b8c088b33ba54891bf853cea
Marjone <marjonedev@gmail.com>
2019-10-11 02:13:32 +0800

    db migrate

--- db/migrate/20191010174021_add_column_emailaccount1011.rb
+++ db/migrate/20191010174021_add_column_emailaccount1011.rb
@@ -1,4 +1,10 @@
 class AddColumnEmailaccount1011 < ActiveRecord::Migration[5.2]
   def change
+    add_column :emailaccounts, :email_provider, :string
+    add_column :emailaccounts, :smtp_host, :string
+    add_column :emailaccounts, :smtp_email, :string
+    add_column :emailaccounts, :smtp_password, :string
+    add_column :emailaccounts, :smtp_port, :string
+    add_column :emailaccounts, :smtp_ssl, :boolean
   end
 end
