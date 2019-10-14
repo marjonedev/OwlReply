c6e811ae7fa8ad9e623b96ec673c9995cd9a0f53
Marjone <marjonedev@gmail.com>
2019-10-15 01:45:26 +0800

    db migration

--- db/migrate/20191014174113_change_column_emailaccount10152019.rb
+++ db/migrate/20191014174113_change_column_emailaccount10152019.rb
@@ -1,4 +1,9 @@
 class ChangeColumnEmailaccount10152019 < ActiveRecord::Migration[5.2]
   def change
+    rename_column :emailaccounts, :smtp_email, :connect_email
+    rename_column :emailaccounts, :smtp_host, :connect_host
+    rename_column :emailaccounts, :smtp_password, :connect_password
+    rename_column :emailaccounts, :smtp_port, :connect_port
+    rename_column :emailaccounts, :smtp_ssl, :connect_ssl
   end
 end
