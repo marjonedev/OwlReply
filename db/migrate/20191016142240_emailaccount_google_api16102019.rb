3bba786edc15d4f7886b793bac667562edd1878b
Marjone <marjonedev@gmail.com>
2019-10-16 22:48:09 +0800

    emailaccounts db migrate

--- db/migrate/20191016142240_emailaccount_google_api16102019.rb
+++ db/migrate/20191016142240_emailaccount_google_api16102019.rb
@@ -1,4 +1,7 @@
 class EmailaccountGoogleApi16102019 < ActiveRecord::Migration[5.2]
   def change
+    add_column :emailaccounts, :google_access_token, :string
+    add_column :emailaccounts, :google_expires_in, :bigint
+    add_column :emailaccounts, :google_refresh_token, :string
   end
 end
