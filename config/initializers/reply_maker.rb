efdd74ada7085255f13044959b5e3fa5e8a71dbc
Marjone <marjonedev@gmail.com>
2019-11-25 13:26:36 +0800

    reply maker changes

--- config/initializers/reply_maker.rb
+++ config/initializers/reply_maker.rb
@@ -74,6 +74,7 @@ module ReplyMaker
     def self.resetting?
       if (REDIS.get("replymaker_reset").to_i == 1)
         REDIS.del("replymaker_reset")
+
         return true
       else
         return false
@@ -159,48 +160,54 @@ module ReplyMaker
       include GoogleConnector
       api = GmailApi.new account
 
-      messages = api.get_messages
+      begin
+        messages = api.get_messages
 
-      ids = []
+        ids = []
 
-      messages.each do |msg|
-        thebody = msg['body'].to_s.downcase
-        next if account.subject_line_skip?(msg['subject'])
-        next if api.is_thread_message! msg['thread_id'] # Skip if this thread has more than one email! Secret sauce!
+        messages.each do |msg|
+          thebody = msg['body'].to_s.downcase
+          next if account.subject_line_skip?(msg['subject'])
+          next if api.is_thread_message! msg['thread_id'] # Skip if this thread has more than one email! Secret sauce!
 
-        auto = ""
-        for reply in account.replies
-          next unless reply.matches?(msg['subject'], thebody)
-          body = reply.body.gsub("\n","<br>\n")
-          auto << body
-          reply.increment!(:drafts_created_today)
-          reply.increment!(:drafts_created_lifetime)
-          reply_used = true
+          auto = ""
+          for reply in account.replies
+            next unless reply.matches?(msg['subject'], thebody)
+            body = reply.body.gsub("\n","<br>\n")
+            auto << body
+            reply.increment!(:drafts_created_today)
+            reply.increment!(:drafts_created_lifetime)
+            reply_used = true
+          end
+
+          body_html = (msg['body_html'].body.to_s rescue "")
+          body_html = (msg['body_text'].body.to_s rescue "") if body_html.strip.blank?
+          body_text = (msg['body_text'].to_s rescue "").strip.blank? ? (msg['body_html'].to_s rescue "") : (msg['body_text'].to_s rescue "")
+          body_html = thebody.gsub("\n","<br>\n") if body_html.blank?
+          body_text = thebody if body_text.blank?
+          email_to = msg['from']
+          subject = "Re: #{msg['subject']}"
+
+          text_part = account.template.gsub("%%reply%%",auto)+"\n\nIn reply to:\n\n"+(body_text)
+          html_part = account.template_html.gsub("%%reply%%",auto)+"<br><br>\n\nOn #{msg['date']}, #{email_to} wrote:<br>\n<br>\n"+body_html
+
+          if reply_used
+            api.create_reply_draft(msg['id'], thread_id: msg['tread_id'], to: email_to, subject: subject, body_text: text_part, body_html: html_part)
+            ids.push(msg['id'])
+          end
+
+          account.increment!(:drafts_created_today)
+          account.increment!(:drafts_created_lifetime)
+          account.increment!(:drafts_missing_replies_today) unless reply_used
+          account.increment!(:drafts_missing_replies_lifetime) unless reply_used
         end
 
-        body_html = (msg['body_html'].body.to_s rescue "")
-        body_html = (msg['body_text'].body.to_s rescue "") if body_html.strip.blank?
-        body_text = (msg['body_text'].to_s rescue "").strip.blank? ? (msg['body_html'].to_s rescue "") : (msg['body_text'].to_s rescue "")
-        body_html = thebody.gsub("\n","<br>\n") if body_html.blank?
-        body_text = thebody if body_text.blank?
-        email_to = msg['from']
-        subject = "Re: #{msg['subject']}"
-
-        text_part = account.template.gsub("%%reply%%",auto)+"\n\nIn reply to:\n\n"+(body_text)
-        html_part = account.template_html.gsub("%%reply%%",auto)+"<br><br>\n\nOn #{msg['date']}, #{email_to} wrote:<br>\n<br>\n"+body_html
-
-        if reply_used
-          api.create_reply_draft(msg['id'], thread_id: msg['tread_id'], to: email_to, subject: subject, body_text: text_part, body_html: html_part)
-          ids.push(msg['id'])
-        end
-
-        account.increment!(:drafts_created_today)
-        account.increment!(:drafts_created_lifetime)
-        account.increment!(:drafts_missing_replies_today) unless reply_used
-        account.increment!(:drafts_missing_replies_lifetime) unless reply_used
+        api.read_messages(ids)
+      rescue Google::Apis::AuthorizationError => exception
+        puts "=================ERRROR==================="
+        puts exception
+        api.refresh_api!
       end
-
-      api.read_messages(ids)
 
     end
 
