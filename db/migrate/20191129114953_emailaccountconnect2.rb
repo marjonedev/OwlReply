bb282f9e2426d42ae5fd2ed8147041094f85d3f6
Marjone <marjonedev@gmail.com>
2019-11-29 19:56:41 +0800

    db migration fix emailaccounts

--- db/migrate/20191129114953_emailaccountconnect2.rb
+++ db/migrate/20191129114953_emailaccountconnect2.rb
@@ -1,4 +1,10 @@
 class Emailaccountconnect2 < ActiveRecord::Migration[5.2]
   def change
+    remove_column  :emailaccounts, :smtp_host
+    remove_column  :emailaccounts, :smtp_port
+    remove_column  :emailaccounts, :smtp_encryption_type
+    add_column :emailaccounts, :imap_host, :string
+    add_column :emailaccounts, :imap_port, :string
+    add_column :emailaccounts, :imap_encryption_type, :string
   end
 end
