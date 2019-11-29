80f1463281d62d91ec3667324030e8a9002a30dd
Marjone <marjonedev@gmail.com>
2019-11-29 21:22:43 +0800

    emailaccount db migrate

--- db/migrate/20191129131658_emailaccountconnect3.rb
+++ db/migrate/20191129131658_emailaccountconnect3.rb
@@ -1,4 +1,6 @@
 class Emailaccountconnect3 < ActiveRecord::Migration[5.2]
   def change
+    add_column :emailaccounts, :imap_ssl, :boolean
+    remove_column :emailaccounts, :imap_encryption_type
   end
 end
