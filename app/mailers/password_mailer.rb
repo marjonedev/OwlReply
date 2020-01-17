4706c33787febde8abcaba99692041206c58c649
Marjone <marjonedev@gmail.com>
2020-01-18 02:44:47 +0800

    email url

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -3,7 +3,9 @@ class PasswordMailer < ActionMailer::Base
 
   def reset_password_email
     @user = params[:user]
-    @url = "https://dev.owlreply.com/password/reset?token=#{@user.reset_password_token}"
+
+    @url = request.original_url
+    @token_url = "https://#{@url}/password/reset?token=#{@user.reset_password_token}"
     mail(to: @user.email_address, subject: 'Reset Your Password')
   end
 end
