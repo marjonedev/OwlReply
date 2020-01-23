b3c07b5be3efcb21d98851d8456e51eb74a1df39
Marjone <marjonedev@gmail.com>
2020-01-24 00:36:24 +0800

    login using email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,7 @@ class SessionsController < ApplicationController
 
     if !!username.match(/\A[\w.+-]+@\w+\.\w+\z/)
       usermail = User.where(email_address: username)
-      # logger.debug "===================================="
-      # logger.debug usermail
+
       if usermail.count > 1
         flash.now[:errormsg] = "Please enter your username, not your email."
         render "new"
@@ -37,17 +36,6 @@ class SessionsController < ApplicationController
       end
     end
 
-    # # user = User.find_by(username: params[:user][:username])
-    # user = User.find_by(username: username)
-    # # authenticate = User.authenticate(params[:user][:username], params[:user][:password])
-    # authenticate = User.authenticate(username, password)
-    # if user && authenticate
-    #   session[:user_id] = user.id
-    #   redirect_to root_url, id: user.id
-    # else
-    #   flash.now[:errormsg] = "Username or password is invalide"
-    #   render "new"
-    # end
   end
 
   def destroy
