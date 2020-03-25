0f5816044281ab2cf5d800353c08a8ebb5a3763d
Marjone <marjonedev@gmail.com>
2020-03-25 21:55:38 +0800

    db migration

--- db/migrate/20200325131135_subscription2.rb
+++ db/migrate/20200325131135_subscription2.rb
@@ -1,4 +1,7 @@
 class Subscription2 < ActiveRecord::Migration[5.2]
   def change
+    remove_column :subscriptions, :featured
+    add_column :subscriptions, :recommended, :boolean, default: false
+    add_column :subscriptions, :feature, :text
   end
 end
