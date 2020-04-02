87fd5f0bd6248a385eb667dfd6e6717b57093569
Marjone <marjonedev@gmail.com>
2020-04-03 02:15:02 +0800

    invoice mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'eco@owlreply.com'
   layout 'mailer'
 end
--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,2 +1,11 @@
 class InvoiceMailer < ApplicationMailer
+  default from: 'ceo@owlreply.com'
+  layout 'invoice_mailer'
+
+  def invoice_created
+    @user = params[:user]
+    @invoice = params[:invoice]
+    mail(to: @user.email_address, subject: "Invoice #{@invoice.number} from OwlReply")
+  end
+
 end
