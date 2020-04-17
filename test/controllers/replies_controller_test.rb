366e74ee201a40b68b4e3d68febe3a47a3041a3b
Marjone <marjonedev@gmail.com>
2020-04-17 23:47:48 +0800

    replies controller testing

--- test/controllers/replies_controller_test.rb
+++ test/controllers/replies_controller_test.rb
@@ -17,34 +17,34 @@ class RepliesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create reply" do
-  #   assert_difference('Reply.count') do
-  #     post replies_url, params: { reply: { body: @reply.body, catchcall: @reply.catchcall, keywords: @reply.keywords, negative_keywords: @reply.negative_keywords } }
-  #   end
-  #
-  #   assert_redirected_to reply_url(Reply.last)
-  # end
-  #
-  # test "should show reply" do
-  #   get reply_url(@reply)
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get edit_reply_url(@reply)
-  #   assert_response :success
-  # end
-  #
-  # test "should update reply" do
-  #   patch reply_url(@reply), params: { reply: { body: @reply.body, catchcall: @reply.catchcall, keywords: @reply.keywords, negative_keywords: @reply.negative_keywords } }
-  #   assert_redirected_to reply_url(@reply)
-  # end
-  #
-  # test "should destroy reply" do
-  #   assert_difference('Reply.count', -1) do
-  #     delete reply_url(@reply)
-  #   end
-  #
-  #   assert_redirected_to replies_url
-  # end
+  test "should create reply" do
+    assert_difference('Reply.count') do
+      post emailaccount_replies_url(@emailaccount), params: { reply: { body: @reply.body, catchcall: @reply.catchcall, keywords: @reply.keywords, negative_keywords: @reply.negative_keywords } }
+    end
+
+    assert_redirected_to reply_url(Reply.last)
+  end
+
+  test "should show reply" do
+    get reply_url(@reply)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_emailaccount_reply_url(@emailaccount, @reply)
+    assert_response :success
+  end
+
+  test "should update reply" do
+    patch emailaccount_reply_url(@emailaccount, @reply), params: { reply: { body: @reply.body, catchcall: @reply.catchcall, keywords: @reply.keywords, negative_keywords: @reply.negative_keywords } }
+    assert_redirected_to reply_url(@reply)
+  end
+
+  test "should destroy reply" do
+    assert_difference('Reply.count', -1) do
+      delete emailaccount_reply_url(@emailaccount, @reply)
+    end
+
+    assert_redirected_to replies_url
+  end
 end
