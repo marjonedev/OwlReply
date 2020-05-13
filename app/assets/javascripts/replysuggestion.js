73a80c4d51427fa7792b19451d567a4aea834a9c
Marjone <marjonedev@gmail.com>
2020-05-14 03:20:30 +0800

    fix inbox keywords

--- app/assets/javascripts/replysuggestion.js
+++ app/assets/javascripts/replysuggestion.js
@@ -1,7 +1,11 @@
 Replymaker = {};
 Replymaker.suggested_keywords = ["Refund","Cancel","Unable","Hours","Hiring","Resume","Human Resources"];
-Replymaker.show_suggestions = function () {
-  $('.keyword_suggestions').remove();
+Replymaker.show_suggestions = function (elem = false) {
+    if(elem){
+        $(elem).find('.keyword_suggestions').remove();
+    }else{
+        $('.keyword_suggestions').remove();
+    }
   var input = $(".new_reply input[name='reply[keywords]']");
   input.after("<div class='keyword_suggestions'></div>");
   for (var i=0;i < Replymaker.suggested_keywords.length;i++) {
@@ -12,20 +16,25 @@ Replymaker.show_suggestions = function () {
     a.setAttribute('class','button button-light button-mini keyword');
     a.setAttribute('data-keyword',text);
     a.addEventListener( 'click', function (e) {
-        Replymaker.add_keyword(e.target.textContent);
-        Replymaker.show_suggestions();
+        Replymaker.add_keyword(e.target.textContent, $(this));
+        var el = $(this).closest('new_reply');
+        Replymaker.show_suggestions(el);
     });
-    $('.keyword_suggestions').append(a);
+      if(elem){
+          $(elem).find('.keyword_suggestions').append(a);
+      }else{
+          $('.keyword_suggestions').append(a);
+      }
   }
 };
 Replymaker.get_suggestions = function () {
   $.get("/replies/suggest");
 };
-Replymaker.add_keyword = function (word) {
+Replymaker.add_keyword = function (word, elem) {
     if ( $.isFunction($.fn.tagsinput) ) {
-        $(".new_reply input[name='reply[keywords]']").tagsinput('add', word);
+        $(elem).closest(".new_reply").find("input[name='reply[keywords]']").tagsinput('add', word);
     }else{
-        let inputKeywords = $(".new_reply input[name='reply[keywords]']");
+        let inputKeywords = $(elem).closest(".new_reply").find("input[name='reply[keywords]']");
         if(inputKeywords.val() == ""){
             inputKeywords.val(word);
         }else{
