ddf67b77302045dbc170e1dcbbd085fb46182cfd
Marjone <marjonedev@gmail.com>
2020-06-11 02:43:11 +0800

    small change on inbox

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -8,43 +8,47 @@ class InboxController < ApplicationController
     @messages = []
     @errors = []
     if @emailaccount.authenticated
-      if @emailaccount.email_provider == "google"
-        api = GmailApi.new @emailaccount
+      if @emailaccount.setupcomplete || @emailaccount.skip_activation
+        if @emailaccount.email_provider == "google"
+          api = GmailApi.new @emailaccount
 
-        result = api.get_messages(limit: (params[:more] ? 20 : 2), unread: (params[:all] ? false : true))
-        messages = result
-        @errors.push("No unread emails.") if messages.empty?
-        @errors.concat(api.errors) if api.errors
-        # We could redirect the user someone based on the error, like back to Google.
+          result = api.get_messages(limit: (params[:more] ? 20 : 2), unread: (params[:all] ? false : true))
+          messages = result
+          @errors.push("No unread emails.") if messages.empty?
+          @errors.concat(api.errors) if api.errors
+          # We could redirect the user someone based on the error, like back to Google.
 
-        messages.each do |msg|
-          date = DateTime.parse(msg['date'])
-          formatted_date = date.strftime('%a, %b %d, %Y at %I:%M %p')
-          subject = msg['subject']
-          from = msg['from']
-          thebody = msg['body_text'].to_s.gsub("\r\n", " ")
-          thebody = thebody.truncate(80, separator: " ")
+          messages.each do |msg|
+            date = DateTime.parse(msg['date'])
+            formatted_date = date.strftime('%a, %b %d, %Y at %I:%M %p')
+            subject = msg['subject']
+            from = msg['from']
+            thebody = msg['body_text'].to_s.gsub("\r\n", " ")
+            thebody = thebody.truncate(80, separator: " ")
 
-          @messages.push({date: formatted_date, subject:subject, body: msg['body'], body_text: thebody, from: from, unread: msg['unread']})
+            @messages.push({date: formatted_date, subject:subject, body: msg['body'], body_text: thebody, from: from, unread: msg['unread']})
+          end
+        else
+
+          api = IMAPApi.new @emailaccount
+          messages = api.get_messages(limit: (params[:more] ? 20 : 2), unread: (params[:all] ? false : true))
+          @errors.push("No unread emails.") if messages.empty?
+          @errors.concat(api.errors) if api.errors
+
+          messages.each do |msg|
+            next if msg['is_thread']
+            date = DateTime.parse(msg['date'])
+            formatted_date = date.strftime('%a, %b %d, %Y at %I:%M %p')
+            subject = msg['subject']
+            from = msg['from']
+            thebody = msg['body_text'].to_s.gsub("\r\n", " ")
+            thebody = thebody.truncate(80, separator: " ")
+
+            @messages.push({date: formatted_date, subject:subject, body: msg['body'], body_text: thebody, from: from, unread: msg['unread']})
+          end
         end
       else
-
-        api = IMAPApi.new @emailaccount
-        messages = api.get_messages(limit: (params[:more] ? 20 : 2), unread: (params[:all] ? false : true))
-        @errors.push("No unread emails.") if messages.empty?
-        @errors.concat(api.errors) if api.errors
-
-        messages.each do |msg|
-          next if msg['is_thread']
-          date = DateTime.parse(msg['date'])
-          formatted_date = date.strftime('%a, %b %d, %Y at %I:%M %p')
-          subject = msg['subject']
-          from = msg['from']
-          thebody = msg['body_text'].to_s.gsub("\r\n", " ")
-          thebody = thebody.truncate(80, separator: " ")
-
-          @messages.push({date: formatted_date, subject:subject, body: msg['body'], body_text: thebody, from: from, unread: msg['unread']})
-        end
+        @errors.push("Email account setup is on progress. Start building your automated replies.")
       end
     else
       @errors.push("Email account is still not connected.")
