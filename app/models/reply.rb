14ed6d9d1f19370b93aae8900f3ef9c18cccb604
Marjone <marjonedev@gmail.com>
2020-09-12 02:22:07 +0800

    fix deprecated drivers and functions

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -42,10 +42,11 @@ class Reply < ApplicationRecord
   def self.body_with_suggestions(b, sug, emailaccount, id)
     b.each {|i|
       unless i.blank?
-        sug.each {|reply|
-          if i.strip == reply
+          sug.each {|reply|
+          if i.downcase.include? reply
             selected = selected(emailaccount, reply) ? 'selected' : ''
-            i.replace "<a class='potential_keyword suggested_#{reply} #{selected}' href='/emailaccounts/#{emailaccount.id}/reply.js?keyword=#{reply}&message=#{id}' data-remote='true' data-disable-with='#{reply}'>#{reply}</a>"
+            i.replace "<a class='potential_keyword suggested_#{reply} #{selected}' href='/emailaccounts/#{emailaccount.id}/reply.js?keyword=#{reply}&message=#{id}' data-remote='true' data-disable-with='#{reply}'>#{i}</a>"
+            break
           end
         }
       end
