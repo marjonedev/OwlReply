e579167ee5b2ce93c19bcde06c94b6e45d1cb04d
Marjone <marjonedev@gmail.com>
2020-12-24 00:01:59 +0800

    imap fix

--- lib/imap_connector.rb
+++ lib/imap_connector.rb
@@ -37,7 +37,7 @@ module IMAPConnector
       begin
         @service.examine(@inbox)
 
-        start_date = 1.week.ago.strftime("%d-%b-%Y") #change to 1.week.ago
+        start_date = 1.day.ago.strftime("%d-%b-%Y") #change to 1.week.ago
 
         tags = ["SINCE", start_date]
         tags.unshift('UNSEEN') if unread
